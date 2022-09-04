bf3dfa08e8e219ec19dced932956f899ddfe97b5
japjeet singh <japjeet612003@gmail.com>
2022-09-04 18:49:00 +0530

    Create latest_automation_with_temperature_display.ino

---
+++ latest_automation_with_temperature_display/latest_automation_with_temperature_display.ino
@@ -0,0 +1,247 @@
+#ifdef ENABLE_DEBUG
+       #define DEBUG_ESP_PORT Serial
+       #define NODEBUG_WEBSOCKETS
+       #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+#include "DHT.h"
+#include <LiquidCrystal_I2C.h>
+
+#include <map>
+LiquidCrystal_I2C lcd(0x27,16,2);     // set the LCD address to 0x27 for a 16 chars and 2 line display
+DHT dht(D3, DHT11);      
+
+#define WIFI_SSID         "JAPJEET SINGH"    
+#define WIFI_PASS         "password"
+#define APP_KEY           "da43*************************"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        "29f******************************************************"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+
+//Enter the device IDs here
+#define device_ID_1   "xxxxxxxxxxxxxxxxxxxxxxxx"
+#define device_ID_2   "6***********************b"
+#define device_ID_3   "6*********************01"
+#define device_ID_4   "61*********************0"
+
+// define the GPIO connected with Relays and switches
+#define RelayPin1 5  //D1
+#define RelayPin2 4  //D2
+#define RelayPin3 14 //D5
+#define RelayPin4 12 //D6
+
+#define SwitchPin1 10  //SD3
+#define SwitchPin2 0   //D3 
+#define SwitchPin3 13  //D7
+#define SwitchPin4 3   //RX
+
+#define wifiLed   16   //D0
+
+// comment the following line if you use a toggle switches instead of tactile buttons
+//#define TACTILE_BUTTON 1
+
+#define BAUD_RATE   9600
+
+#define DEBOUNCE_TIME 250
+
+typedef struct {      // struct for the std::map below
+  int relayPIN;
+  int flipSwitchPIN;
+} deviceConfig_t;
+
+
+// this is the main configuration
+// please put in your deviceId, the PIN for Relay and PIN for flipSwitch
+// this can be up to N devices...depending on how much pin's available on your device ;)
+// right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
+std::map<String, deviceConfig_t> devices = {
+    //{deviceId, {relayPIN,  flipSwitchPIN}}
+    {device_ID_1, {  RelayPin1, SwitchPin1 }},
+    {device_ID_2, {  RelayPin2, SwitchPin2 }},
+    {device_ID_3, {  RelayPin3, SwitchPin3 }},
+    {device_ID_4, {  RelayPin4, SwitchPin4 }}     
+};
+
+typedef struct {      // struct for the std::map below
+  String deviceId;
+  bool lastFlipSwitchState;
+  unsigned long lastFlipSwitchChange;
+} flipSwitchConfig_t;
+
+std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
+                                                  // it will be setup in "setupFlipSwitches" function, using informations from devices map
+
+void setupRelays() { 
+  for (auto &device : devices) {           // for each device (relay, flipSwitch combination)
+    int relayPIN = device.second.relayPIN; // get the relay pin
+    pinMode(relayPIN, OUTPUT);             // set relay pin to OUTPUT
+    digitalWrite(relayPIN, LOW);
+  }
+}
+
+void setupFlipSwitches() {
+  for (auto &device : devices)  {                     // for each device (relay / flipSwitch combination)
+    flipSwitchConfig_t flipSwitchConfig;              // create a new flipSwitch configuration
+
+    flipSwitchConfig.deviceId = device.first;         // set the deviceId
+    flipSwitchConfig.lastFlipSwitchChange = 0;        // set debounce time
+    flipSwitchConfig.lastFlipSwitchState = true;     // set lastFlipSwitchState to false (LOW)--
+
+    int flipSwitchPIN = device.second.flipSwitchPIN;  // get the flipSwitchPIN
+
+    flipSwitches[flipSwitchPIN] = flipSwitchConfig;   // save the flipSwitch config to flipSwitches map
+    pinMode(flipSwitchPIN, INPUT_PULLUP);                   // set the flipSwitch pin to INPUT
+  }
+}
+
+bool onPowerState(String deviceId, bool &state)
+{
+  Serial.printf("%s: %s\r\n", deviceId.c_str(), state ? "on" : "off");
+  int relayPIN = devices[deviceId].relayPIN; // get the relay pin for corresponding device
+  digitalWrite(relayPIN, !state);             // set the new relay state
+  return true;
+}
+
+void handleFlipSwitches() {
+  unsigned long actualMillis = millis();                                          // get actual millis
+  for (auto &flipSwitch : flipSwitches) {                                         // for each flipSwitch in flipSwitches map
+    unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange;  // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
+
+    if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME) {                    // if time is > debounce time...
+
+      int flipSwitchPIN = flipSwitch.first;                                       // get the flipSwitch pin from configuration
+      bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState;           // get the lastFlipSwitchState
+      bool flipSwitchState = digitalRead(flipSwitchPIN);                          // read the current flipSwitch state
+      if (flipSwitchState != lastFlipSwitchState) {                               // if the flipSwitchState has changed...
+#ifdef TACTILE_BUTTON
+        if (flipSwitchState) {                                                    // if the tactile button is pressed 
+#endif      
+          flipSwitch.second.lastFlipSwitchChange = actualMillis;                  // update lastFlipSwitchChange time
+          String deviceId = flipSwitch.second.deviceId;                           // get the deviceId from config
+          int relayPIN = devices[deviceId].relayPIN;                              // get the relayPIN from config
+          bool newRelayState = !digitalRead(relayPIN);                            // set the new relay State
+          digitalWrite(relayPIN, newRelayState);                                  // set the trelay to the new state
+
+          SinricProSwitch &mySwitch = SinricPro[deviceId];                        // get Switch device from SinricPro
+          mySwitch.sendPowerStateEvent(!newRelayState);                            // send the event
+#ifdef TACTILE_BUTTON
+        }
+#endif      
+        flipSwitch.second.lastFlipSwitchState = flipSwitchState;                  // update lastFlipSwitchState
+      }
+    }
+  }
+}
+
+void setupWiFi()
+{
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.printf(".");
+    delay(250);
+  }
+  digitalWrite(wifiLed, LOW);
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+void setupSinricPro()
+{
+  for (auto &device : devices)
+  {
+    const char *deviceId = device.first.c_str();
+    SinricProSwitch &mySwitch = SinricPro[deviceId];
+    mySwitch.onPowerState(onPowerState);
+  }
+
+  SinricPro.begin(APP_KEY, APP_SECRET);
+  SinricPro.restoreDeviceStates(true);
+}
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);
+
+  pinMode(wifiLed, OUTPUT);
+  digitalWrite(wifiLed, HIGH);
+
+  setupRelays();
+  setupFlipSwitches();
+  setupWiFi();
+  setupSinricPro();
+
+  lcd.begin();                      // initialize the lcd 
+  lcd.backlight();
+  Serial.begin(9600);
+  Serial.println(F("DHT11 Started!"));
+  // For Writing the Welcome screen on the LCD
+  // Print a message to the LCD.
+  lcd.setCursor(4,0);
+  lcd.print("WELCOME");
+  lcd.setCursor(7,1);
+  lcd.print("TO");
+  delay(1000);
+  lcd.setCursor(2,0);
+  lcd.print("TEMPERATURE");
+  lcd.setCursor(4,1);
+  lcd.print("MONITOR");
+  delay(2000);
+  dht.begin();
+}
+
+void loop()
+{
+  SinricPro.handle();
+  handleFlipSwitches();
+  
+  delay(2000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print("Not Found");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  // for Output in Serial Monitor
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  // Print Values to the LCD.
+  lcd.setCursor(0, 0);
+  lcd.print("Temp  : ");
+  lcd.print(t);
+  lcd.print((char)223);// for Degree(°) 
+  lcd.print("C   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Humid : ");
+  lcd.print(h);
+  lcd.print("%");
+}
